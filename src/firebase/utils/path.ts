60fcf8a1458b2aac0bcbdaf11f7f33e49e54d123
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-03-13 23:34:42 +0700

    chore: remove log on firebase:utils

--- src/firebase/utils/path.ts
+++ src/firebase/utils/path.ts
@@ -1,16 +1,13 @@
 import type Firebase from "firebase";
-import debug from "debug";
 
 import { USER_PATH, VERSION_PATH } from "../constants/path";
 import { isExist } from "./checker";
 
-const logger = debug("firebase").extend("utils");
 export const buildRef = (firebase: typeof Firebase, ...path: string[]): Firebase.database.Reference => {
   const userPath = [USER_PATH, firebase.auth().currentUser?.uid, VERSION_PATH]
     .concat(...path)
     .filter(v => isExist(v))
     .join("/");
 
-  logger("buildRef", userPath);
   return firebase.database().ref(userPath);
 };
