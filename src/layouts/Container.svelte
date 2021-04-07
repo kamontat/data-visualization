25e3a0d78d6cf679f212f381d4436083e651da4b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-04-07 16:30:48 +0700

    Update Container.svelte

--- src/layouts/Container.svelte
+++ src/layouts/Container.svelte
@@ -7,8 +7,6 @@
     display: flex;
     flex-direction: column;
 
-    margin-top: var(--space-md);
-    margin-bottom: var(--space-md);
     margin-left: var(--space-xl);
     margin-right: var(--space-xl);
   }
